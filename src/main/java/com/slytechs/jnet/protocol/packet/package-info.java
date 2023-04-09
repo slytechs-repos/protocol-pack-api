90553ea49d6d1baab1c93d369f52e7aaa7be90da
Sly Technologies <116599131+slytechs-repos@users.noreply.github.com>
2023-04-09 17:11:42 -0400

    Delete source directory.
    The current code is in the 'develop' branch and is not ready to be presented in 'main' at this time

--- src/main/java/com/slytechs/jnet/protocol/packet/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provides high level packet services. Packets are very common and low level
- * capture routines deliver packets in raw data form. This packet package,
- * provides high level packet services which allow the programmer to use a high
- * level API such as checking for presents of protocol headers and other high
- * level services (ie. IP fragment reassembly).
- */
-package com.slytechs.jnet.protocol.packet;
