db90b7cb8f4bd3c9d19b51ddfece2d4a61b720d0
Mark Bednarczyk <mark@slytechs.com>
2023-04-11 11:06:17 -0400

    Added skeleton PPP header

---
+++ src/main/java/com/slytechs/protocol/pack/core/Ppp.java
@@ -0,0 +1,51 @@
+/*
+ * Sly Technologies Free License
+ * 
+ * Copyright 2023 Sly Technologies Inc.
+ *
+ * Licensed under the Sly Technologies Free License (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.slytechs.com/free-license-text
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.slytechs.protocol.pack.core;
+
+import java.util.concurrent.locks.Lock;
+
+import com.slytechs.protocol.Header;
+import com.slytechs.protocol.pack.core.constants.CoreIdTable;
+
+/**
+ * Point to Point protocol (PPP).
+ * 
+ * @author Sly Technologies Inc
+ * @author repos@slytechs.com
+ *
+ */
+public class Ppp extends Header {
+
+	public static int ID = CoreIdTable.CORE_ID_PPP;
+
+	/**
+	 * @param id
+	 */
+	public Ppp() {
+		super(ID);
+	}
+
+	/**
+	 * @param id
+	 * @param lock
+	 */
+	public Ppp(Lock lock) {
+		super(ID, lock);
+	}
+
+}
