0fa2f9afebd75635b9a07bb4b2f7425ce1d17402
Mark Bednarczyk <mark@slytechs.com>
2023-04-11 11:24:54 -0400

    Added Ethernet header unit tests

---
+++ src/test/java/com/slytechs/protocol/pack/core/TestEthernetHeader.java
@@ -0,0 +1,111 @@
+/*
+ * Sly Technologies Free License
+ * 
+ * Copyright 2023 Sly Technologies Inc.
+ *
+ * Licensed under the Sly Technologies Free License (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.slytechs.com/free-license-text
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.slytechs.protocol.pack.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.slytechs.protocol.HeaderNotFound;
+import com.slytechs.protocol.descriptor.PacketDissector;
+import com.slytechs.protocol.pack.core.constants.CoreConstants;
+import com.slytechs.protocol.pack.core.constants.PacketDescriptorType;
+import com.slytechs.protocol.runtime.util.HexStrings;
+
+/**
+ * VLAN header tests
+ * 
+ * @author Sly Technologies Inc
+ * @author repos@slytechs.com
+ * @author Mark Bednarczyk
+ *
+ */
+@Tag("osi-layer2")
+@Tag("ethernet")
+class TestEthernetHeader {
+
+	static final PacketDissector DISSECTOR = PacketDissector
+			.dissector(PacketDescriptorType.TYPE2);
+
+	static final ByteBuffer DESC_BUFFER = ByteBuffer
+			.allocateDirect(CoreConstants.DESC_TYPE2_BYTE_SIZE_MAX)
+			.order(ByteOrder.nativeOrder());
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		DISSECTOR.reset();
+
+		DESC_BUFFER.clear();
+		while (DESC_BUFFER.remaining() > 0)
+			DESC_BUFFER.put((byte) 0);
+
+		DESC_BUFFER.clear();
+	}
+
+	@Test
+	void test_Ethernet_destination() throws HeaderNotFound {
+		var packet = PacketDefinition.VLAN.toPacket();
+		packet.descriptor().bind(DESC_BUFFER);
+
+		DISSECTOR.dissectPacket(packet);
+		DISSECTOR.writeDescriptor(packet.descriptor());
+
+		var ethernet = packet.getHeader(new Ethernet());
+		
+		var EXPECTED_MAC = HexStrings.parseHexString("0060089fb1f3");
+
+		assertArrayEquals(EXPECTED_MAC, ethernet.destination());
+	}
+
+	@Test
+	void test_Ethernet_source() throws HeaderNotFound {
+		var packet = PacketDefinition.VLAN.toPacket();
+		packet.descriptor().bind(DESC_BUFFER);
+
+		DISSECTOR.dissectPacket(packet);
+		DISSECTOR.writeDescriptor(packet.descriptor());
+
+		var ethernet = packet.getHeader(new Ethernet());
+		
+		var EXPECTED_MAC = HexStrings.parseHexString("00400540ef24");
+
+		assertArrayEquals(EXPECTED_MAC, ethernet.source());
+	}
+
+	@Test
+	void test_Ethernet_type() throws HeaderNotFound {
+		var packet = PacketDefinition.VLAN.toPacket();
+		packet.descriptor().bind(DESC_BUFFER);
+
+		DISSECTOR.dissectPacket(packet);
+		DISSECTOR.writeDescriptor(packet.descriptor());
+
+		var ethernet = packet.getHeader(new Ethernet());
+		
+		assertEquals(0x8100, ethernet.type());
+	}
+
+}
